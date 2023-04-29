b7a27534bfd5b660ac59556bf50adb165a6952d8
John McGlumphy <jrmcglumphy@gmail.com>
2023-04-29 13:14:12 -0500

    Start of spawner

---
+++ Assets/Spawning.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    public GameObject spawns;
+    public bool stopLoop = false;
+    public float spawntime;
+    public float spawndelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
